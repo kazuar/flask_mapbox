8858e90cb0cdc934c35d4c1bf85bc870d3e97006
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-10-15 20:45:20 -0400

    Flask application structure

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_object(__name__)
+app.config.from_envvar('APP_CONFIG_FILE', silent=True)
 
-app.run(threaded=True)
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+# app.run(threaded=True)
---
+++ start.sh
@@ -0,0 +1,6 @@
+# start.sh
+
+export FLASK_APP=server.py
+export FLASK_DEBUG=1
+export APP_CONFIG_FILE=settings.py
+flask run
