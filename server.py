69aba25fe6f720737e41acde00efdd488ee29fc2
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-12-30 10:04:54 -0500

    Use only one set of locations instead of two for both html and javascript

--- server.py
+++ server.py
@@ -37,38 +37,45 @@ ROUTE = [
 ROUTE_URL = "https://api.mapbox.com/directions/v5/mapbox/driving/{0}.json?access_token={1}&overview=full&geometries=geojson"
 
 def create_route_url():
+    # Creat a string containing all the geo coordinates
     lat_longs = ";".join(["{0},{1}".format(point["long"], point["lat"]) for point in ROUTE])
+    # Create the url with the geo coordinates and access token
     url = ROUTE_URL.format(lat_longs, MAPBOX_ACCESS_KEY)
     return url
 
-def create_stop_location_detail(title, latitude, longitude, index):
+def create_stop_location_detail(title, latitude, longitude, index, route_index):
     point = Point([longitude, latitude])
     properties = {
         "title": title,
         'icon': "campsite",
         'marker-color': '#3bb2d0',
-        'marker-symbol': index
+        'marker-symbol': index,
+        'route_index': route_index
     }
     feature = Feature(geometry = point, properties = properties)
     return feature
 
 def create_stop_locations_details():
     stop_locations = []
-    for location in ROUTE:
+    for route_index, location in enumerate(ROUTE):
         if not location["is_stop_location"]:
             continue
         stop_location = create_stop_location_detail(
             location['name'],
             location['lat'],
             location['long'],
-            len(stop_locations) + 1
+            len(stop_locations) + 1,
+            route_index
         )
         stop_locations.append(stop_location)
     return stop_locations
 
 def get_route_data():
+    # Get the route url
     route_url = create_route_url()
+    # Perform a GET request to the route API
     result = requests.get(route_url)
+    # Convert the return value to JSON
     data = result.json()
 
     geometry = data["routes"][0]["geometry"]
@@ -96,17 +103,14 @@ def mapbox_js():
 def mapbox_gl():
     route_data, waypoints = get_route_data()
 
-    stop_locations = []
-    for waypoint_index, waypoint in enumerate(waypoints):
-        stop_location = ROUTE[waypoint_index]
-        if stop_location["is_stop_location"]:
-            location_index = route_data['geometry']['coordinates'].index(waypoint["location"])
-            stop_location["location_index"] = location_index
-            stop_locations.append(stop_location)
+    stop_locations = create_stop_locations_details()
+    for stop_location in stop_locations:
+        waypoint_index = stop_location.properties["route_index"]
+        waypoint = waypoints[waypoint_index]
+        stop_location.properties["location_index"] = route_data['geometry']['coordinates'].index(waypoint["location"])
 
     return render_template('mapbox_gl.html', 
         ACCESS_KEY=MAPBOX_ACCESS_KEY,
         route_data = route_data,
-        stop_locations = stop_locations,
-        stop_locations_json = json.dumps(stop_locations)
+        stop_locations = stop_locations
     )
